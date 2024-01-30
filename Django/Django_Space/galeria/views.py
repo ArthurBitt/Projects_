54b79b26c97736114c2a5b471e12a3608704b8b8
ArthurBitt <add2coffee@gmail.com>
2024-01-30 06:31:34 -0300

    .

--- Django/Django_Space/galeria/views.py
+++ Django/Django_Space/galeria/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from galeria.models import Fotografia
 
 
 def getindexresponse(request):
 # *getindexresponse Recebe uma requisição request e retorna um html com uma func response #
-    
-    dados = {
-        1:{"Nome": "Nebulosa de Carina",
-        "Legenda": "webbtelescope.org/ NASA / James Webb"},
-        2:{"Nome": "Galáxia NGC 1079",
-        "Legenda": "nasa.org/ NASA / Hubble"}
-    }
+    query = Fotografia.objects.all()
+    return render(request,'template_galeria/index.html', {"cards": query})
 
-
-    return render(request, 'template_galeria\index.html', {"cards": dados})
 def imagem(request):
-    return render(request, 'template_galeria\imagem.html')
-# Create your views here.
+    return render(request, 'template_galeria/imagem.html')
+# Create your views here
--- Django/Django_Space/setup/settings.py
+++ Django/Django_Space/setup/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #* boa prática usar esse formato derivado de .apps e
     'galeria.apps.GaleriaConfig'
 ]
 
